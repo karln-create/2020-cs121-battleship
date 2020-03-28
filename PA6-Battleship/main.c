209caec248dad09fd82330343ef5137166966ac7
karln-create <khoa.norden@gmail.com>
2020-03-28 10:26:56 -0700

    main
    mainheader

---
+++ PA6-Battleship/main.c
@@ -0,0 +1,14 @@
+/*
+	NAME: KARL NORDEN | CLASS: CPTS 121, SECTION 17
+	DESCRIPTION: BUILD A BATTLESHIP GAME IN C.
+				Refer to README.md for more information and complete program development.
+*/
+
+#include "mainheader.h"
+
+int main(void)
+{
+
+
+	return 0;
+}
---
+++ PA6-Battleship/mainheader.h
@@ -0,0 +1,8 @@
+#ifndef MAINHEADER_H
+#define MAINHEADER_H
+#define _CRT_SECURE_NO_WARNING
+
+#include <stdio.h>
+#include <string.h>
+
+#endif
